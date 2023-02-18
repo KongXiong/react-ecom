96034d3b936d7cb196f9e692c13f397b1dadaf49
Kong Xiong <kongxiong@Kongs-MBP.attlocal.net>
2023-02-18 12:11:16 -0600

    initial commit

---
+++ client/src/scenes/global/Footer.js
@@ -0,0 +1,65 @@
+import { Box, Typography, useTheme } from '@mui/material'
+import { shades } from '../../theme'
+
+const Footer = () => {
+  const {
+    palette: { neutral },
+  } = useTheme()
+
+  return (
+    <Box mt="70px" p="40px 0" backgroundColor={neutral.light}>
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        flexWrap="wrap"
+        rowGap="30px"
+        columnGap="clamp(20px, 30px, 40px)"
+      >
+        <Box width="clamp(20%, 30%, 40%)">
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            mb="30px"
+            color={shades.secondary[500]}
+          >
+            ECOMMER
+          </Typography>
+          <div>
+            This is something or other. Some things will usually be here to tell
+            people things and whatnot.
+          </div>
+        </Box>
+        <Box>
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            About Us
+          </Typography>
+          <Typography mb="30px">Careers</Typography>
+          <Typography mb="30px">Our Stores</Typography>
+          <Typography mb="30px">Terms & Conditions</Typography>
+          <Typography mb="30px">Privacy Policy</Typography>
+        </Box>
+        <Box>
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            Customer Care
+          </Typography>
+          <Typography mb="30px">Help Center</Typography>
+          <Typography mb="30px">Track Your Order</Typography>
+          <Typography mb="30px">Corporate & Bulk Purchasing</Typography>
+          <Typography mb="30px">Return & Refunds</Typography>
+        </Box>
+        <Box width="clamp(20%, 25%, 30%)">
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            Contact Us
+          </Typography>
+          <Typography mb="30px">100 N. South St, Milwaukee WI 12345</Typography>
+          <Typography mb="30px">Email: 1234@email.com</Typography>
+          <Typography mb="30px">(123)123-1234</Typography>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default Footer
